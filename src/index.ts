0a0bba8bc0a334d0524dcef847f19b56207322c3
acarters <54558076+acarters@users.noreply.github.com>
2023-10-18 21:23:46 -0400

    Spaghetti code baseline functionality :-)
    Bot now scrapes mastodon for Walt Ruff posts, does some regexp operations to get the plaintext in a workable format, and posts them on bluesky. Code style is horrendous, but it seems to work on a base level if you squint?

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import Bot from "./lib/bot.js";
 import getPostText from "./lib/getPostText.js";
 
-const text = await Bot.run(getPostText, { dryRun: true });
+const text = await Bot.run(getPostText);
 
 console.log(`[${new Date().toISOString()}] Posted: "${text}"`);
--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -1,4 +1,26 @@
+import * as Mastodon from 'tsl-mastodon-api';
+
+
+const mastodon = new Mastodon.API({access_token: 'PRZhmwmS5fpkXo442UE8SGHv8TL7XOiqjhpOh49heb0', api_url: 'https://mastodon.social/api/v1/'});
+
 export default async function getPostText() {
+//var account = await mastodon.getAccount();
+
+var account = await mastodon.getStatuses("109764698354053424", {'limit':1});
+var string = JSON.stringify(account);
+var parse = JSON.parse(string);
+var jsonObj = parse["json"];
+var string2 = JSON.stringify(jsonObj);
+var parse2 = JSON.parse(string2);
+var jsonObj2 = parse2[0]["content"];
+var string3 = JSON.stringify(jsonObj2);
+var reg = new RegExp("<(:?[^>]+)>", "g");
+var split = string3.replace(reg, "");
+split = split.slice(1,-1);
+console.log(`string3: ${string3}`);
+console.log(`split: ${split}`);
+
+
   // Generate the text for your post here. You can return a string or a promise that resolves to a string
-  return "Hello from the Bluesky API";
+  return split;
 }
