47a1439888ec514ffea5cbc2028903f777bcf009
acarters <54558076+acarters@users.noreply.github.com>
2023-10-19 11:16:56 -0400

    br and p formatting
    regex now swaps an empty p block with 2 new lines, a br block with 1 new line.

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -14,8 +14,9 @@ var parse2 = JSON.parse(string2);
 var jsonObj2 = parse2[0]["content"];
 var string3 = JSON.stringify(jsonObj2);
 var reg = new RegExp("<(:?[^>]+)>", "g");
-var bulletReg = new RegExp("•", "g");
-var split = string3.replace(reg, "").replace(bulletReg, "\n•"); // Use the regex to remove the HTML formatting from the mastodon content. 
+var pReg = new RegExp("</p><p>", "g");
+var brReg = new RegExp("<br>", "g");
+var split = string3.replace(pReg, "\n\n").replace(brReg, "\n").replace(reg, ""); // Use the regex to remove the HTML formatting from the mastodon 
 split = split.slice(1,-1); // Remove the quotation marks.
   return split;
 }
