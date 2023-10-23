c8d6e3245a5ff042faeb5f68f7d56add25f14b42
acarters <54558076+acarters@users.noreply.github.com>
2023-10-23 14:08:04 -0400

    Naive handler for tweets that are too long
    I forgot that Elon Musk made it to where people can write essays as tweets, so I have added functionality where the bot can parse these into multiple posts. Ideally I will have the subsequent post chunks be replies to the original, but I will need to look into Bluesky API before I do this. This should be a naive fix in the meantime.

--- src/lib/bot.ts
+++ src/lib/bot.ts
@@ -88,7 +88,29 @@ export default class Bot {
     if (!dryRun) {
       for (let i = 10 - 1; i >= 0; i--) 
       {
-        await bot.post(mastodonArr[i]);
+        //var img = {imgpath: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fchorus.fm%2Fwp-content%2Fuploads%2F2016%2F05%2Fcc.jpg&f=1&nofb=1&ipt=ad629262928e0166e2f43e1535d9fbeb6f4f33ba24064c97eb667f47a0a26266&ipo=images', $type: 'image/jpeg', alttext: 'A jagged bolt of lightning strikes a large building',}
+
+        if (mastodonArr[i].length <= 300)
+        {
+          await bot.post(mastodonArr[i]);//, embed: img});
+        }
+        else
+        {
+          var postLen = mastodonArr[i].length;
+          var numberOfPosts = ~~(postLen / 294) + 1;
+          console.log("sliced! %d\n", numberOfPosts);
+
+          for (let j = 0; j < numberOfPosts; j++)
+          {
+          console.log("(" + j * 295 + "," + (j * 295 + 294) + ")\n");
+          var partialStr = mastodonArr[i].slice((j * 294), (j * 295 + 294)) + " [" + (j + 1) + "/" + numberOfPosts + "]";
+          /*if (j != 0)
+          {
+            partialStr = '"' + partialStr; 
+          }*/
+          await bot.post(partialStr);
+          }
+        }
       }
     }
     return mastodonArr[0];
