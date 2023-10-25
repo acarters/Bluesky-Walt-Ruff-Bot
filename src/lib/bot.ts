3285845dccc5a4cd9a0a402c24dea3e5ec83796b
acarters <54558076+acarters@users.noreply.github.com>
2023-10-25 13:41:18 -0400

    Long posts parse into reply threads
    Long posts that need more than one post to be posted on bsky now post their multiple posts as a thread under the original, instead of creating separate posts. Changed the bot.post signature to enable a bool arg value that tells the post whether or not to post this post as an original post, or as a reply to the previous post. Root URI and CID are kept as public variables to the class.

--- src/lib/bot.ts
+++ src/lib/bot.ts
@@ -14,6 +14,8 @@ type BotOptions = {
 
 export default class Bot {
   #agent;
+  public rootCid: string;
+  public rootUri: string;
 
   static defaultOptions: BotOptions = {
     service: bskyService,
@@ -22,6 +24,8 @@ export default class Bot {
 
   constructor(service: AtpAgentOpts["service"]) {
     this.#agent = new BskyAgent({ service });
+    this.rootCid = "";
+    this.rootUri = "";
   }
 
   login(loginOpts: AtpAgentLoginOpts) {
@@ -29,44 +33,83 @@ export default class Bot {
   }
 
   async post(
+    isReply: boolean,
     text:
       | string
       | (Partial<AppBskyFeedPost.Record> &
           Omit<AppBskyFeedPost.Record, "createdAt">)
-  ) {
-
+  ) 
+  {
+    console.log("isReply: " + isReply + "\n");
     var bskyFeedAwait = await this.#agent.getAuthorFeed({actor: "notwaltruff.bsky.social", limit: 12,});
     var bskyFeed = bskyFeedAwait["data"]["feed"];
-    console.log('bskyFeed: %s\n', bskyFeed);
-    var stringArr = [];
-  for (let i = 0; i < 10; i++) 
-  {
-    console.log(i);
-    var bsky0 = bskyFeed[i];
-    var bsky = bsky0["post"]["record"];
-    var bskyArr = Object.entries(bsky);
-    var bskyText = bskyArr[0][1];
-    if (text === bskyText || text === "")
-    {
-      console.log("failed on %d\n", i);
-      return "37";
-    }
-    stringArr.push(bskyText);
-  }
-  console.log('Bluesky string array: %s\n', stringArr);
+    var bskyFeed0 = bskyFeed[0];
+    var bskyPosts = bskyFeed0["post"];
+    var parentId = {uri: "", cid: ""};
+      if (isReply == false)
+      {
+        this.rootUri = "";
+        this.rootCid = "";
+      }
+      else
+      {
+        var bskyUri = bskyPosts["uri"];
+        var bskyCid = bskyPosts["cid"];
+        parentId = {uri: bskyUri, cid: bskyCid};
+        if (this.rootUri == "")
+        {
+          this.rootUri = bskyUri;
+          this.rootCid = bskyCid;
+          //console.log("parentId: %s\n", parentId);
+        }
+      }
 
-    console.log(text === stringArr[0])
-
-
-      if (typeof text === "string") {
+      var stringArr = [];
+      for (let i = 0; i < 10; i++) 
+      {
+        //console.log(i);
+        var bsky0 = bskyFeed[i];
+        var bsky = bsky0["post"]["record"];
+        var bskyArr = Object.entries(bsky);
+        var bskyText = bskyArr[0][1];
+        if (text === bskyText || text === "")
+        {
+          //console.log("failed on %d\n", i);
+          return "37";
+        }
+        stringArr.push(bskyText);
+      }
+      //console.log('Bluesky string array: %s\n', stringArr);
+      if (typeof text === "string") 
+      {
         const richText = new RichText({text});
         await richText.detectFacets(this.#agent);
-        const record = {
-          text: richText.text,
-          facets: richText.facets,
-        };
+        var record;
+        if (isReply == true)
+        {
+          var rootId = {uri: this.rootUri, cid: this.rootCid};
+          console.log("parentId: {uri: " + parentId["uri"] + ", cid: " + parentId["cid"] + "}\n");
+          console.log("rootId: {uri: " + rootId["uri"] + ", cid: " + rootId["cid"] + "}\n");
+          record = 
+          {
+            text: richText.text,
+            facets: richText.facets,
+            reply: {root: rootId, parent: parentId,},
+          };
+        }
+        else
+        {
+          record = 
+          {
+            text: richText.text,
+            facets: richText.facets,
+          };
+        }
+        console.log(text);
         return this.#agent.post(record);
-      } else {
+      } 
+      else 
+      {
         return this.#agent.post(text);
       }
   }
@@ -81,7 +124,7 @@ export default class Bot {
     const bot = new Bot(service);
     await bot.login(bskyAccount);
     const mastodonAwait = await getPostText();
-    const mastodonArr = mastodonAwait.split("\/");
+    var mastodonArr = mastodonAwait.split("\/");
 
     console.log('Mastodon string array: %s\n', mastodonArr);
 
@@ -92,7 +135,7 @@ export default class Bot {
 
         if (mastodonArr[i].length <= 300)
         {
-          await bot.post(mastodonArr[i]);//, embed: img});
+          await bot.post(false, mastodonArr[i]);//, embed: img});
         }
         else
         {
@@ -102,13 +145,16 @@ export default class Bot {
 
           for (let j = 0; j < numberOfPosts; j++)
           {
-          console.log("(" + j * 295 + "," + (j * 295 + 294) + ")\n");
-          var partialStr = mastodonArr[i].slice((j * 294), (j * 295 + 294)) + " [" + (j + 1) + "/" + numberOfPosts + "]";
-          /*if (j != 0)
-          {
-            partialStr = '"' + partialStr; 
-          }*/
-          await bot.post(partialStr);
+          console.log("(" + j * 294 + "," + (j * 294 + 294) + ")\n");
+          var partialStr = mastodonArr[i].slice((j * 294), (j * 294 + 294)) + " [" + (j + 1) + "/" + numberOfPosts + "]";
+            if (j == 0)
+            {
+              await bot.post(false, partialStr);
+            }
+            else
+            {
+              await bot.post(true, partialStr);
+            }
           }
         }
       }
